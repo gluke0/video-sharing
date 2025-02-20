c5e908f902eec1f49d896fb72afcd4f730b9ad46
gluke0 <milanesigianluca@gmail.com>
2025-02-20 16:29:57 +0100

    js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let totVideos = document.querySelectorAll('video');
 let sidebar = document.querySelector('.left_section');
+let sideItems = document.querySelectorAll('.sidebar .item');
+let cateItems = document.querySelectorAll('.categories a');
+
+// play/pause hover
