c977d2728849558d456a9ccb26306b6239b94884
gluke0 <milanesigianluca@gmail.com>
2025-02-20 16:52:49 +0100

    hover video js

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let sideItems = document.querySelectorAll('.sidebar .item');
 let cateItems = document.querySelectorAll('.categories a');
 
 // play/pause hover
+totVideos.forEach(video =>{
+    video.addEventListener('mouseover',() =>{
+        video.play();
+    });
+    video.addEventListener('mouseleave',() =>{
+        video.pause();
+    });
+});
